70216184ffc7bdf762cb589fd7deea6109810074
Julian Bici <julianbici@gmail.com>
2020-09-27 07:44:30 -0400

    Loading data from json file

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Groceries',
-            budget: 300
-        },
-        {
-            title: 'Tuition',
-            budget: 1000
-        },
-        {
-            title: 'Travel',
-            budget: 500
-        }
-    ]
-};
+const budget = require('./budget.json')
  
 app.use('/', express.static('public'));
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}')
+    console.log('Example app listening at http://localhost:3000')
 });
